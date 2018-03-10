7e94def747c60eff7cf5f5ebd4944b389a1cae37
Benjamin Konsemüller <ben@Benjamins-Mac.local>
2018-03-10 06:53:03 -0800

    iOS Compression

--- ios/Classes/FlutterNativeImagePlugin.m
+++ ios/Classes/FlutterNativeImagePlugin.m
@@ -3,72 +3,58 @@
 
 @implementation FlutterNativeImagePlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"flutter_native_image"
-            binaryMessenger:[registrar messenger]];
-  FlutterNativeImagePlugin* instance = [[FlutterNativeImagePlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"flutter_native_image"
+                                     binaryMessenger:[registrar messenger]];
+    FlutterNativeImagePlugin* instance = [[FlutterNativeImagePlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
     NSDictionary *_arguments;
+    
     if ([@"getPlatformVersion" isEqualToString:call.method]) {
         result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    }
     else if ([@"compressImage" isEqualToString:call.method]) {
         _arguments = call.arguments;
-        NSString file = [[_arguments objectForKey:@"file"] stringValue]
+        NSString *fileExtension = @"_compressed.jpg";
+        NSString *file = [[_arguments objectForKey:@"file"] stringValue];
+        NSString *fileName = [[file lastPathComponent] stringByDeletingPathExtension];
+        
+        NSString *temp =  [fileName stringByAppendingString:fileExtension];
+        
+        NSURL *furl = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingPathComponent:temp]];
+        
         int quality = [[_arguments objectForKey:@"quality"] intValue];
         int percentage = [[_arguments objectForKey:@"percentage"] intValue];
-    
-        NSData *data = [NSData dataWithContentsOfURL:file];
-        UIImage img = [[UIImage alloc] initWithData:data];
-        NSData* compressed = UIImageJPEGRepresentation(img, quality / 100);
-
+        
+        NSURL *url = [NSURL URLWithString:file];
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        UIImage *img = [[UIImage alloc] initWithData:data];
+        
+        if (img.size.height > 1500 || img.size.width > 1500) {
+            CGFloat newWidth = (img.size.width / 100 * percentage);
+            CGFloat newHeight = (img.size.height / 100 * percentage);
+            
+            CGSize newSize = CGSizeMake(newWidth, newHeight);
+            UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
+            
+            [img drawInRect:CGRectMake(0, 0, newWidth, newWidth)];
+            
+            UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+            UIGraphicsEndImageContext();
+            [UIImageJPEGRepresentation(newImage, quality / 100) writeToFile:furl.absoluteString atomically:YES];
+            result(furl.absoluteString);
+            return;
+        }
+        
+        [UIImageJPEGRepresentation(img, quality / 100) writeToFile:furl.absoluteString atomically:YES];
+        
+        result(furl.absoluteString);
+        return;
     } else {
         result(FlutterMethodNotImplemented);
     }
 }
-
-+(UIImage *)compressImage:(UIImage *)image{
-    float actualHeight = image.size.height;
-    float actualWidth = image.size.width;
-    float maxHeight = 1136.0f;
-    float maxWidth = 640.0f;
-    float imgRatio = actualWidth/actualHeight;
-    float maxRatio = maxWidth/maxHeight;
-    float compressionQuality = 1;//50 percent compression
-
-    if (actualHeight > maxHeight || actualWidth > maxWidth){
-        if(imgRatio < maxRatio){
-            //adjust width according to maxHeight
-            imgRatio = maxHeight / actualHeight;
-            actualWidth = imgRatio * actualWidth;
-            actualHeight = maxHeight;
-        }
-        else if(imgRatio > maxRatio){
-            //adjust height according to maxWidth
-            imgRatio = maxWidth / actualWidth;
-            actualHeight = imgRatio * actualHeight;
-            actualWidth = maxWidth;
-        }
-        else{
-            actualHeight = maxHeight;
-            actualWidth = maxWidth;
-        }
-    }else{
-        actualHeight = maxHeight;
-        actualWidth = maxWidth;
-        compressionQuality = 1;
-    }
-
-    CGRect rect = CGRectMake(0.0, 0.0, actualWidth, actualHeight);
-    UIGraphicsBeginImageContext(rect.size);
-    [image drawInRect:rect];
-    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
-    NSData *imageData = UIImageJPEGRepresentation(img, compressionQuality);
-    UIGraphicsEndImageContext();
-
-    return [UIImage imageWithData:imageData];
-}
-
 @end
