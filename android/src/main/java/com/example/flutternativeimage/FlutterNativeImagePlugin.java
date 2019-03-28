96c1e5ab3723a25dbe5e1364b8f2295ad221c2e4
lau pong <lauppong90@gmail.com>
2019-03-28 15:08:28 +0800

    update the exif

--- android/src/main/java/com/example/flutternativeimage/FlutterNativeImagePlugin.java
+++ android/src/main/java/com/example/flutternativeimage/FlutterNativeImagePlugin.java
@@ -1,5 +1,6 @@
 package com.example.flutternativeimage;
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.ExifInterface;
@@ -15,11 +16,15 @@ import java.io.OutputStream;
 
 import java.lang.IllegalArgumentException;
 
+import id.zelory.compressor.Compressor;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,208 +35,233 @@ import java.util.HashMap;
  * FlutterNativeImagePlugin
  */
 public class FlutterNativeImagePlugin implements MethodCallHandler {
-  private final Activity activity;
-  /**
-   * Plugin registration.
-   */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_native_image");
-    channel.setMethodCallHandler(new FlutterNativeImagePlugin(registrar.activity()));
-  }
+    private final Activity activity;
+    public static final String TAG = FlutterNativeImagePlugin.class.getSimpleName();
 
-  private FlutterNativeImagePlugin(Activity activity) {
-    this.activity = activity;
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if(call.method.equals("compressImage")) {
-      String fileName = call.argument("file");
-      int resizePercentage = call.argument("percentage");
-      int targetWidth = call.argument("targetWidth") == null ? 0 : (int) call.argument("targetWidth");
-      int targetHeight = call.argument("targetHeight") == null ? 0 : (int) call.argument("targetHeight");
-      int quality = call.argument("quality");
-
-      File file = new File(fileName);
-
-      if(!file.exists()) {
-        result.error("file does not exist", fileName, null);
-        return;
-      }
-
-      Bitmap bmp = BitmapFactory.decodeFile(fileName);
-      ByteArrayOutputStream bos = new ByteArrayOutputStream();
-
-      int newWidth = targetWidth == 0 ? (bmp.getWidth() / 100 * resizePercentage) : targetWidth;
-      int newHeight = targetHeight == 0 ? (bmp.getHeight() / 100 * resizePercentage) : targetHeight;
-
-      bmp = Bitmap.createScaledBitmap(
-              bmp, newWidth, newHeight, false);
-
-      bmp.compress(Bitmap.CompressFormat.JPEG, quality, bos);
-
-      try {
-        String outputFileName = File.createTempFile(
-          getFilenameWithoutExtension(file).concat("_compressed"), 
-          ".jpg", 
-          activity.getExternalCacheDir()
-        ).getPath();
-
-        OutputStream outputStream = new FileOutputStream(outputFileName);
-        bos.writeTo(outputStream);
-
-        copyExif(fileName, outputFileName);
-
-        result.success(outputFileName);
-      } catch (FileNotFoundException e) {
-        e.printStackTrace();
-        result.error("file does not exist", fileName, null);
-      } catch (IOException e) {
-        e.printStackTrace();
-        result.error("something went wrong", fileName, null);
-      }
-
-      return;
+    /**
+     * Plugin registration.
+     */
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_native_image");
+        channel.setMethodCallHandler(new FlutterNativeImagePlugin(registrar.activity()));
     }
-    if(call.method.equals("getImageProperties")) {
-      String fileName = call.argument("file");
-      File file = new File(fileName);
 
-      if(!file.exists()) {
-        result.error("file does not exist", fileName, null);
-        return;
-      }
-
-      BitmapFactory.Options options = new BitmapFactory.Options();
-      options.inJustDecodeBounds = true;
-      BitmapFactory.decodeFile(fileName,options);
-      HashMap<String, Integer> properties = new HashMap<String, Integer>();
-      properties.put("width", options.outWidth);
-      properties.put("height", options.outHeight);
-
-      int orientation = ExifInterface.ORIENTATION_UNDEFINED;
-      try {
-        ExifInterface exif = new ExifInterface(fileName);
-        orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
-      } catch(IOException ex) {
-        // EXIF could not be read from the file; ignore
-      }
-      properties.put("orientation", orientation);
-
-      result.success(properties);
-      return;
+    private FlutterNativeImagePlugin(Activity activity) {
+        this.activity = activity;
     }
-    if(call.method.equals("cropImage")) {
-    	String fileName = call.argument("file");
-    	int originX = call.argument("originX");
-    	int originY = call.argument("originY");
-    	int width = call.argument("width");
-    	int height = call.argument("height");
 
-      File file = new File(fileName);
+    @SuppressLint("CheckResult")
+    @Override
+    public void onMethodCall(MethodCall call, final Result result) {
+        if (call.method.equals("compressImage")) {
+            final String fileName = call.argument("file");
+            int resizePercentage = call.argument("percentage");
+            int targetWidth = call.argument("targetWidth") == null ? 0 : (int) call.argument("targetWidth");
+            int targetHeight = call.argument("targetHeight") == null ? 0 : (int) call.argument("targetHeight");
+            int quality = call.argument("quality");
 
-    	if(!file.exists()) {
-  			result.error("file does not exist", fileName, null);
-  			return;
-  		}
+            File file = new File(fileName);
 
-    	Bitmap bmp = BitmapFactory.decodeFile(fileName);
-      ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            if (!file.exists()) {
+                result.error("file does not exist", fileName, null);
+                return;
+            }
 
-    	try {
-			 bmp = Bitmap.createBitmap(bmp, originX, originY, width, height);
-      } catch(IllegalArgumentException e) {
-        e.printStackTrace();
-        result.error("bounds are outside of the dimensions of the source image", fileName, null);
-      }
+//            Bitmap bmp = BitmapFactory.decodeFile(fileName);
+//            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+//
+//            int newWidth = targetWidth == 0 ? (bmp.getWidth() / 100 * resizePercentage) : targetWidth;
+//            int newHeight = targetHeight == 0 ? (bmp.getHeight() / 100 * resizePercentage) : targetHeight;
+//
+//            bmp = Bitmap.createScaledBitmap(
+//                    bmp, newWidth, newHeight, false);
+//
+//            bmp.compress(Bitmap.CompressFormat.JPEG, quality, bos);
+//
+//            try {
+//                String outputFileName = File.createTempFile(
+//                        getFilenameWithoutExtension(file).concat("_compressed"),
+//                        ".jpg",
+//                        activity.getExternalCacheDir()
+//                ).getPath();
+//
+//                OutputStream outputStream = new FileOutputStream(outputFileName);
+//                bos.writeTo(outputStream);
+//
+//                copyExif(fileName, outputFileName);
+//
+//                result.success(outputFileName);
+//            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//                result.error("file does not exist", fileName, null);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//                result.error("something went wrong", fileName, null);
+//            }
+            Bitmap bmp = BitmapFactory.decodeFile(fileName);
+            int newWidth = targetWidth == 0 ? (bmp.getWidth() / 100 * resizePercentage) : targetWidth;
+            int newHeight = targetHeight == 0 ? (bmp.getHeight() / 100 * resizePercentage) : targetHeight;
+            new Compressor(activity)
+                    .setMaxWidth(newWidth)
+                    .setMaxHeight(newHeight)
+                    .setQuality(quality)
+                    .compressToFileAsFlowable(file)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Consumer<File>() {
+                        @Override
+                        public void accept(File file) {
+                            result.success(file.getAbsolutePath());
+                        }
+                    }, new Consumer<Throwable>() {
+                        @Override
+                        public void accept(Throwable throwable) {
+                            throwable.printStackTrace();
+                            result.error("something went wrong", fileName, null);
+                        }
+                    });
 
-      bmp.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+            return;
+        }
+        if (call.method.equals("getImageProperties")) {
+            String fileName = call.argument("file");
+            File file = new File(fileName);
 
-    	try {
-        String outputFileName = File.createTempFile(
-          getFilenameWithoutExtension(file).concat("_cropped"), 
-          ".jpg", 
-          activity.getExternalCacheDir()
-        ).getPath();
+            if (!file.exists()) {
+                result.error("file does not exist", fileName, null);
+                return;
+            }
 
-  			OutputStream outputStream = new FileOutputStream(outputFileName);
-  			bos.writeTo(outputStream);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(fileName, options);
+            HashMap<String, Integer> properties = new HashMap<String, Integer>();
+            properties.put("width", options.outWidth);
+            properties.put("height", options.outHeight);
 
-        copyExif(fileName, outputFileName);
+            int orientation = ExifInterface.ORIENTATION_UNDEFINED;
+            try {
+                ExifInterface exif = new ExifInterface(fileName);
+                orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
+            } catch (IOException ex) {
+                // EXIF could not be read from the file; ignore
+            }
+            properties.put("orientation", orientation);
 
-        result.success(outputFileName);
-  		} catch (FileNotFoundException e) {
-  			e.printStackTrace();
-        result.error("file does not exist", fileName, null);
-  		} catch (IOException e) {
-  			e.printStackTrace();
-        result.error("something went wrong", fileName, null);
-  		}
+            result.success(properties);
+            return;
+        }
+        if (call.method.equals("cropImage")) {
+            String fileName = call.argument("file");
+            int originX = call.argument("originX");
+            int originY = call.argument("originY");
+            int width = call.argument("width");
+            int height = call.argument("height");
 
-  		return;
+            File file = new File(fileName);
+
+            if (!file.exists()) {
+                result.error("file does not exist", fileName, null);
+                return;
+            }
+
+            Bitmap bmp = BitmapFactory.decodeFile(fileName);
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+            try {
+                bmp = Bitmap.createBitmap(bmp, originX, originY, width, height);
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+                result.error("bounds are outside of the dimensions of the source image", fileName, null);
+            }
+
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+
+            try {
+                String outputFileName = File.createTempFile(
+                        getFilenameWithoutExtension(file).concat("_cropped"),
+                        ".jpg",
+                        activity.getExternalCacheDir()
+                ).getPath();
+
+                OutputStream outputStream = new FileOutputStream(outputFileName);
+                bos.writeTo(outputStream);
+
+                copyExif(fileName, outputFileName);
+
+                result.success(outputFileName);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                result.error("file does not exist", fileName, null);
+            } catch (IOException e) {
+                e.printStackTrace();
+                result.error("something went wrong", fileName, null);
+            }
+
+            return;
+        }
+        if (call.method.equals("getPlatformVersion")) {
+            result.success("Android " + android.os.Build.VERSION.RELEASE);
+        } else {
+            result.notImplemented();
+        }
     }
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+
+    private void copyExif(String filePathOri, String filePathDest) {
+        try {
+            ExifInterface oldExif = new ExifInterface(filePathOri);
+            ExifInterface newExif = new ExifInterface(filePathDest);
+
+            List<String> attributes =
+                    Arrays.asList(
+                            "FNumber",
+                            "ExposureTime",
+                            "ISOSpeedRatings",
+                            "GPSAltitude",
+                            "GPSAltitudeRef",
+                            "FocalLength",
+                            "GPSDateStamp",
+                            "WhiteBalance",
+                            "GPSProcessingMethod",
+                            "GPSTimeStamp",
+                            "DateTime",
+                            "Flash",
+                            "GPSLatitude",
+                            "GPSLatitudeRef",
+                            "GPSLongitude",
+                            "GPSLongitudeRef",
+                            "Make",
+                            "Model",
+                            "Orientation");
+            for (String attribute : attributes) {
+                setIfNotNull(oldExif, newExif, attribute);
+            }
+
+            newExif.saveAttributes();
+
+        } catch (Exception ex) {
+            Log.e(TAG, "Error preserving Exif data on selected image: " + ex);
+        }
     }
-  }
 
-  private void copyExif(String filePathOri, String filePathDest) {
-    try {
-      ExifInterface oldExif = new ExifInterface(filePathOri);
-      ExifInterface newExif = new ExifInterface(filePathDest);
-
-      List<String> attributes =
-          Arrays.asList(
-              "FNumber",
-              "ExposureTime",
-              "ISOSpeedRatings",
-              "GPSAltitude",
-              "GPSAltitudeRef",
-              "FocalLength",
-              "GPSDateStamp",
-              "WhiteBalance",
-              "GPSProcessingMethod",
-              "GPSTimeStamp",
-              "DateTime",
-              "Flash",
-              "GPSLatitude",
-              "GPSLatitudeRef",
-              "GPSLongitude",
-              "GPSLongitudeRef",
-              "Make",
-              "Model",
-              "Orientation");
-      for (String attribute : attributes) {
-        setIfNotNull(oldExif, newExif, attribute);
-      }
-
-      newExif.saveAttributes();
-
-    } catch (Exception ex) {
-      Log.e("FlutterNativeImagePlugin", "Error preserving Exif data on selected image: " + ex);
+    private void setIfNotNull(ExifInterface oldExif, ExifInterface newExif, String property) {
+        if (oldExif.getAttribute(property) != null) {
+            newExif.setAttribute(property, oldExif.getAttribute(property));
+        }
     }
-  }
 
-  private void setIfNotNull(ExifInterface oldExif, ExifInterface newExif, String property) {
-    if (oldExif.getAttribute(property) != null) {
-      newExif.setAttribute(property, oldExif.getAttribute(property));
+    private static String pathComponent(String filename) {
+        int i = filename.lastIndexOf(File.separator);
+        return (i > -1) ? filename.substring(0, i) : filename;
     }
-  }
 
-  private static String pathComponent(String filename) {
-    int i = filename.lastIndexOf(File.separator);
-    return (i > -1) ? filename.substring(0, i) : filename;
-  }
+    private static String getFilenameWithoutExtension(File file) {
+        String fileName = file.getName();
 
-  private static String getFilenameWithoutExtension(File file) {
-    String fileName = file.getName();
-
-    if (fileName.indexOf(".") > 0) {
-      return fileName.substring(0, fileName.lastIndexOf("."));
-    } else {
-      return fileName;
+        if (fileName.indexOf(".") > 0) {
+            return fileName.substring(0, fileName.lastIndexOf("."));
+        } else {
+            return fileName;
+        }
     }
-  }
 }
